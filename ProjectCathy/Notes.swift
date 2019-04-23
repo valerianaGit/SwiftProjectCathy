14dd53e248c6d1c1eb8c3419c4cad4a72e0ce1e7
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-04-23 12:39:50 -0500

    First commit 1

---
+++ ProjectCathy/Notes.swift
@@ -0,0 +1,28 @@
+//
+//  Notes.swift
+//  ProjectCathy
+//
+//  Created by Valeria Duran on 4/19/19.
+//  Copyright © 2019 Valeria Duran. All rights reserved.
+//
+
+import Foundation
+
+class Notes {
+    var stack: String? //stacks are collections of folders... ARRAY OF NOTES, so probably need to make a Stack model and 
+    var tag: [String]?  //can be many tags//WILL PROBABLY NEED TO BE OF TYPE TAG. Array of tags
+    var title: String?
+    var content: Any? // canbe string or image, drawing, anything the user needs to store
+    
+    init?(stack: String?, tag: [String]?, title: String?, content: Any?) {
+        
+        if tag!.isEmpty {
+            return nil
+        }
+        self.stack = stack
+        self.title = title
+        self.tag = tag
+        self.content = content
+    }
+    
+}
---
+++ ProjectCathy/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ProjectCathy
+//
+//  Created by Valeria Duran on 4/19/19.
+//  Copyright © 2019 Valeria Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Collection View Protocols
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+}
+
